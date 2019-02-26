bba0bb50a04f754271f482c6449d668f8e8f11b7
katakisnik <nikif31@hotmail.com>
2019-02-26 15:48:42 +0200

    2.0

--- testapi/src/main/java/test/ShopRepository.java
+++ testapi/src/main/java/test/ShopRepository.java
@@ -1,9 +1,14 @@
 package test;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import test.Shop;
 
 public interface ShopRepository extends CrudRepository<Shop, Integer> {
+	
+	public List<Shop> findBywithdrawn(Integer withdrawn);
+
 
 }
