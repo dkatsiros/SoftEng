5ad47cc03cb93a5abc04461314e0849b2ee5be3a
Vxiros <xiros.vas@gmail.com>
2019-03-01 19:10:35 +0200

    msgggg

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -19,24 +19,24 @@ export class RegisterComponent implements OnInit {
       name:['',[Validators.required]],
       email:['',[Validators.required]],
       password:['',[Validators.required]]
-      
+
     })
   }
    // convenience getter for easy access to form fields
   get f() { return this.userRegister.controls; }
-  
+
   onSubmit(){
 
     if (this.userRegister.valid){
       console.log(this.f.name);
-      this.reg.register(this.f.username,this.f.name,this.f.email,this.f.password); 
+      this.reg.register(this.f.username,this.f.name,this.f.email,this.f.password);
       alert('User form Validsasas');
     }
     else{
       console.log(this.f.name);
       alert('User form Invalid');
     }
-    
+
     console.log("Submited");
   }
 
